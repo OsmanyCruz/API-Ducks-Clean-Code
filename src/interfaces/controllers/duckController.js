0245f011cc669624837478ad7c330f1ae9e78c52
OsmanyCruz <osmanycruzgarcia@gmail.com>
2024-11-13 11:24:34 -0300

    ajuste getRandomDuckUseCase

--- src/interfaces/controllers/duckController.js
+++ src/interfaces/controllers/duckController.js
@@ -1,27 +1,16 @@
 class DuckController {
-    constructor(getRandomDuckUseCase, getDucksByNumberUseCase) {
-      this.getRandomDuckUseCase = getRandomDuckUseCase;
-      this.getDucksByNumberUseCase = getDucksByNumberUseCase;
-    }
-  
-    async getRandomDuck(req, res) {
-      try {
-        const url = await this.getRandomDuckUseCase.execute();
-        res.json({ url });
-      } catch (error) {
-        res.status(500).json({ error: error.message });
-      }
-    }
-  
-    async getDucksByNumber(req, res) {
-      try {
-        const number = parseInt(req.params.number, 10);
-        const urls = await this.getDucksByNumberUseCase.execute(number);
-        res.json({ urls });
-      } catch (error) {
-        res.status(500).json({ error: error.message });
-      }
+  constructor(getRandomDuckUseCase) {
+    this.getRandomDuckUseCase = getRandomDuckUseCase;
+  }
+
+  async getRandomDuck(req, res) {
+    try {
+      const url = await this.getRandomDuckUseCase.execute();
+      res.json({ url });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
     }
   }
-  
-  module.exports = DuckController;
+}
+
+module.exports = DuckController;
--- src/usecases/getRandomDuckUseCase.js
+++ src/usecases/getRandomDuckUseCase.js
@@ -1,13 +1,25 @@
-class getRandomDuckUseCase {
-    constructor(duckRepository) {
-      this.duckRepository = duckRepository;
-    }
-  
-    async execute() {
-      const duckList = await this.duckRepository.fetchDuckList();
-      const randomDuck = duckList[Math.floor(Math.random() * duckList.length)];
-      return `https://random-d.uk/api/${randomDuck.url}`;
-    }
+class GetRandomDuckUseCase {
+  constructor(duckRepository) {
+    this.duckRepository = duckRepository;
   }
-  
-  module.exports = getRandomDuckUseCase;
+
+  async execute() {
+    const { images, gifs } = await this.duckRepository.fetchDuckList();
+
+    // Combinar ambos arrays
+    const allItems = [...images, ...gifs];
+
+    if (allItems.length === 0) {
+      throw new Error("No ducks available to select.");
+    }
+
+    // Seleccionar un elemento aleatorio
+    const randomIndex = Math.floor(Math.random() * allItems.length);
+    const randomDuck = allItems[randomIndex];
+
+    // Generar la URL completa
+    return `https://random-d.uk/api/${randomDuck}`;
+  }
+}
+
+module.exports = GetRandomDuckUseCase;
